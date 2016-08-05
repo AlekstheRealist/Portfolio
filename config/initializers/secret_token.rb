e47cce92ab60829df036e37e87a54bb49e13ab79
Aleks Real <aleksanderreal@gmail.com>
2016-08-05 01:35:42 +0000

    Initial commit and installed Twitter bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Portfolio::Application.config.secret_key_base = '940c115a32e4fa483e4655d541bc256bd1578cb6a0d1a37a26fcb5d6925f56b0cccdb86d0692fc4798ce7d9a15b1708dd12a25f60d121b7461a6b3051bd1027f'
