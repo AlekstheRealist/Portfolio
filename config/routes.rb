6942dd8d60063f446e720624032d0370b8cc2ef2
Aleks Real <aleksanderreal@gmail.com>
2016-08-18 23:03:23 +0000

    Added root page with static_page controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Portfolio::Application.routes.draw do
+  root 'static_page#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
